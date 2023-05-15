e58818a622e4dfc423f4c384b114119d64935015
buiduchieu <duchieubui25012002@gmail.com>
2023-05-15 21:10:08 +0700

    add engines in package.json

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const app = require("express")();
-const mongoose = require("mongoose");
-const foodRoutes = require("./routes/foodRoutes.js");
+// const app = require("express")();
+// const mongoose = require("mongoose");
+// const foodRoutes = require("./routes/foodRoutes.js");
 
-mongoose.connect(
-	"mongodb+srv://duchieubui25012002:<123abc123aye@@>@cluster0.42svaum.mongodb.net/test",
-	{
-	useNewUrlParser: true,
-	useUnifiedTopology: true
-	}
-);
+// mongoose.connect(
+// 	"mongodb+srv://duchieubui25012002:<123abc123aye@@>@cluster0.42svaum.mongodb.net/test",
+// 	{
+// 	useNewUrlParser: true,
+// 	useUnifiedTopology: true
+// 	}
+// );
 
-app.use(foodRoutes);
-app.get("/",(req,res) => {
-    res.send("Nhom 2 sieu cap vu tru");
-})
-app.listen(3000, ()=>{
-	console.log("Server is running...");
-})
+// app.use(foodRoutes);
+// app.get("/",(req,res) => {
+//     res.send("Nhom 2 sieu cap vu tru");
+// })
+// app.listen(3000, ()=>{
+// 	console.log("Server is running...");
+// })
